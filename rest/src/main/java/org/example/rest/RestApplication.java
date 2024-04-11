541211dbcf5beebaaa2ead436afc6be88b398c7c
Fedor Matorin <f.matorin@mail.ru>
2024-04-11 14:29:55 +0300

    Добален api gateway

--- rest/src/main/java/org/example/rest/RestApplication.java
+++ rest/src/main/java/org/example/rest/RestApplication.java
@@ -2,10 +2,12 @@ package org.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class RestApplication {
 
     public static void main(String[] args) {
